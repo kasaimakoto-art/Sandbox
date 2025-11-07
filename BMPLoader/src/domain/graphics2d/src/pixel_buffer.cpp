80d73457ca7cfee356a4008687752b53b4770ca1
life-obscura <sine.rdj@gmail.com>
2025-11-08 01:50:47 +0900

    Image and PixelBuffer refactor: replace Pixel array with PixelBuffer, enhance validity checks, and update pixel manipulation functions.

--- BMPLoader/src/domain/graphics2d/src/pixel_buffer.cpp
+++ BMPLoader/src/domain/graphics2d/src/pixel_buffer.cpp
@@ -2,14 +2,41 @@
 
 namespace kaf::domain::graphics2d{
     PixelBuffer::PixelBuffer(const size_t size, const Pixel& pixel) {
-        if(size_ > 0 || pixels_ != nullptr)
-            return;
+        if(size_ > 0 || pixels_ != nullptr) { pixels_ = nullptr; }
+        std::unique_ptr<Pixel[]> pixelArray = std::make_unique<Pixel[]>(size);
+        if(!pixelArray) { return; }
+        size_ = size;
+        std::fill(pixelArray.get(), pixelArray.get() + size, pixel);
+        pixels_ = std::move(pixelArray);
     }
 
-    bool setPixels(PixelBuffer& target, const std::unique_ptr<Pixel[]>&& pixelArray, const size_t size, const size_t startPos){
-
+    bool setPixels(PixelBuffer& target, const std::unique_ptr<Pixel[]>& pixelArray, const size_t size, const size_t startPos){
+        if(target.isValid() && pixelArray && size > 0 && startPos + size <= target.size_){
+            std::copy(pixelArray.get(), pixelArray.get() + size, target.pixels_.get() + startPos);
+            return true;
+        }
+        return false;
     }
 
     bool setPixels(PixelBuffer& target, const std::vector<Pixel>& pixelList, const size_t startPos) {
+        if(target.isValid() && !pixelList.empty() && startPos + pixelList.size() <= target.size_){
+            std::copy(pixelList.begin(), pixelList.end(), target.pixels_.get() + startPos);
+            return true;
+        }
+        return false;
+    }
+    bool setPixel(PixelBuffer& target, const Pixel& pixel, const size_t pos){
+        if(target.isValid() && pos < target.size_){
+            target.pixels_[pos] = pixel;
+            return true;
+        }
+        return false;
+    }
+    bool getPixel(const PixelBuffer& buffer, Pixel& pixel, const size_t pos){
+        if(buffer.isValid() && pos < buffer.size_){
+            pixel = buffer.pixels_[pos];
+            return true;
+        }
+        return false;
     }
 }
